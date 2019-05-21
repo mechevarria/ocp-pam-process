a56c7a3eadc5daf143bc83db9dd3f317b90aad38
Mike Echevarria <mechevar@redhat.com>
2019-05-21 14:08:00 -0400

    export from openshift

--- git-clone.sh
+++ git-clone.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
 # make sure ssh-port-forward.sh is running
-
+echo "Default password for pamAdmin is redhatpam1!"
 git clone ssh://pamAdmin@localhost:8001/MySpace/malware
+
+rm -rf src
+rm -rf global
+cd malware
+mv * ../
+cd ..
+rm -rf malware
